e97bc41b4116c12f2c1b950439602712227803a6
Prabeen04 <prabeen.strange@gmail.com>
2018-03-03 22:01:19 +0530

    form events added

--- src/container/add_user.js
+++ src/container/add_user.js
@@ -3,12 +3,22 @@ import React from 'react';
 class AddUser extends React.Component{
     constructor(props){
         super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
+    handleSubmit = (e) => {
+        console.log(e.target.value)
+    }
+    handleChange = (e) => {
+        console.log(e.target.value)
+    }
     render(){
         return(
             <div>
                 <h3>Add User</h3>
+                <input type="text" onChange={this.handleChange}/>
+               <button onClick={this.handleSubmit}>Add User</button> 
             </div>
         );
     }
