1a7fa208ab16fd770a87e2693e0fb1eafb621174
Prabeen04 <prabeen.strange@gmail.com>
2018-03-08 09:16:28 +0530

    menu items for navbar added

--- src/components/layouts/navbar/navbar.js
+++ src/components/layouts/navbar/navbar.js
@@ -2,35 +2,37 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import NavMenu from '../navmenu/navmenu'
 
-class Navbar extends React.Component{
-    constructor(props){
+class Navbar extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {open: false};
+        this.state = { open: false };
         this.handleClose = this.handleClose.bind(this);
         this.handleToggle = this.handleToggle.bind(this);
     }
-    handleClose = () => this.setState({open: false});
-    handleToggle = () => this.setState({open: !this.state.open});
-    render(){
-        return(<div>
-             <AppBar
-            title="REDUXXX"
-            onLeftIconButtonClick ={ () => this.handleToggle()}
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-          />
-          {/* Left side Drawer */}
-          <Drawer
-            docked={false}
-            width={200}
-            open={this.state.open}
-            onRequestChange={(open) => this.setState({open})}
+    handleClose = () => this.setState({ open: false });
+    handleToggle = () => this.setState({ open: !this.state.open });
+    render() {
+        return (<div>
+            {/* top appbar */}
+            <AppBar
+                title="REDUXXX"
+                onLeftIconButtonClick={() => this.handleToggle()}
+            />
+            {/* Left side Drawer */}
+            <Drawer
+                docked={false}
+                width={200}
+                open={this.state.open}
+                onRequestChange={(open) => this.setState({ open })}
             >
-            <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
-            <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
-        </Drawer>
+                {/* <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+                <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem> */}
+                <NavMenu/>
+            </Drawer>
         </div>
-           
+
         );
     }
 }
