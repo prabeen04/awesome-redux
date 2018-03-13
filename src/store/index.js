2e781fbea5c0844f3de931274712aa4860275d6a
Prabeen04 <prabeen.strange@gmail.com>
2018-03-13 18:49:56 +0530

    asyncMiddleware created

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from '../reducers/index';
 import thunk from 'redux-thunk';
+import { AsyncMiddleware } from '../middlewares/async'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk), applyMiddleware(AsyncMiddleware));
 
 export default store;
