43911e54267807de1bd662d082f5d47cd7d411ad
Prabeen04 <prabeen.strange@gmail.com>
2018-03-15 21:03:53 +0530

    chartjs-2 implemented

--- src/container/dashboard/dashboard.js
+++ src/container/dashboard/dashboard.js
@@ -2,7 +2,8 @@ import React from 'react';
 import UserList from '../user_list';
 import UserDetail from '../user_detail';
 import AddUser from '../add_user';
-
+import LineChart from '../../utility/chartjs/line_chart';
+import BarChart from '../../utility/chartjs/bar_chart';
 class DashBoard extends React.Component {
     constructor(props) {
         super(props)
@@ -15,22 +16,28 @@ class DashBoard extends React.Component {
                 <UserDetail/>
                 <AddUser/> */}
                     <div className="flex-container jumpstart-box">
-                        <h3>Jump start</h3>
+                        <h2>0.0 €</h2>
+                        <h3>Negotiation Stage</h3>
                     </div>
                     <div className="flex-container jumpstart-box">
-                        <h3>Jump start</h3>
+                        <h2>125.0 €</h2>
+                        <h3>Funnel Value</h3>
                     </div>
                     <div className="flex-container jumpstart-box">
-                        <h3>Jump start</h3>
+                        <h2>43.7 %</h2>
+                        <h3>Project Margin</h3>
                     </div>
                     <div className="flex-container jumpstart-box">
-                        <h3>Jump start</h3>
+                        <h2>45.6%</h2>
+                        <h3>Sales Forecast</h3>
                     </div>
                     <div className="flex-container jumpstart-box">
-                        <h3>Jump start</h3>
+                        <h2>110.0 €</h2>
+                        <h3>Approved Expenses</h3>
                     </div>
                     <div className="flex-container jumpstart-box">
-                        <h3>Jump start</h3>
+                        <h2>5050.0 €</h2>
+                        <h3>Approved Mileage</h3>
                     </div>
                 </div>
                 <div className="flex-container">
@@ -38,10 +45,10 @@ class DashBoard extends React.Component {
                 <UserDetail/>
                 <AddUser/> */}
                     <div className="flex-container chart-box">
-                        <h3>CHART COMPONENT</h3>
+                       <LineChart />
                     </div>
                     <div className="flex-container chart-box">
-                        <h3>CHART COMPONENT</h3>
+                        <BarChart />
                     </div>
                     <div className="flex-container chart-box">
                         <h3>CHART COMPONENT</h3>
