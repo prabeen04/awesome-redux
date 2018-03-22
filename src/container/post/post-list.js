3a158124ddbcbeeb1d484960742897a4a0e482fb
Prabeen04 <prabeen.strange@gmail.com>
2018-03-22 09:09:58 +0530

    redux action and dispatch called

--- src/container/post/post-list.js
+++ src/container/post/post-list.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { conect } from 'react-redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getPosts } from '../../actions/posts_action';
 
 class PostList extends React.Component{
     constructor(props){
         super(props)
     }
 
+    componentWillMount(){
+        console.log('inside post CWM');
+        this.props.getPosts();
+    }
+    componentDidMount(){
+        console.log(this.props.posts);
+    }
     render(){
         return(
             <h1>post list</h1>
         );
     }
+}
+
+const mapStateToProps = (state) => {
+    console.log(state)
+    return{
+        posts: state.posts
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        getPosts: getPosts
+    }, dispatch);
 }
 export default connect(mapStateToProps, mapDispatchToProps)(PostList);
