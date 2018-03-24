6e9023e9d0615019b5dbb74cef381a88fb6873ce
Prabeen04 <prabeen.strange@gmail.com>
2018-03-24 21:13:50 +0530

    dialog moved to add post

--- src/container/post/add_post.js
+++ src/container/post/add_post.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
+import Dialog from 'material-ui/Dialog';
+import RaisedButton from 'material-ui/RaisedButton';
 import { Form, Input, TextArea } from 'semantic-ui-react'
-
+import './post.css'
 class AddPost extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            open: false,
+        };
     }
+
+    handleOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
 
     render() {
         const renderField = ({ input, label, type, meta: { touched, error }, ...custom }) => (
@@ -39,13 +53,13 @@ class AddPost extends Component {
         return (
             <div>
                 <form onSubmit={handleSubmit}>
-                    <div>
+                    <div className="post-input">
                         <Field name="author" label="Author" component={renderField} />
                     </div>
-                    <div>
+                    <div className="post-input">
                         <Field name="title" label="Post Title" component={renderField}  />
                     </div>
-                    <div>
+                    <div className="post-input">
                         <Field name="body" label="Your Story" component={renderTextarea}  style={{width: '100%'}} />
                     </div>
                     <button type="submit">Submit</button>
