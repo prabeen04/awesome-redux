c42b37a4d554533278f4ec9fa4cd6bbd1a81585c
Prabeen04 <prabeen.strange@gmail.com>
2018-03-24 20:25:06 +0530

    material dialog implemented in post container

--- src/container/post/post.js
+++ src/container/post/post.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import PostList from './post_list';
 import AddPost from './add_post';
+import Dialog from 'material-ui/Dialog';
+import RaisedButton from 'material-ui/RaisedButton';
 
-class Posts extends React.Component{
-    constructor(props){
+class Posts extends React.Component {
+    constructor(props) {
         super(props)
+
+        this.state = {
+            open: false,
+        };
     }
 
-    render(){
-        return(
+    handleOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        const actions = [
+            <RaisedButton
+                label="Cancel"
+                primary={true}
+                onClick={this.handleClose}
+            />,
+            <RaisedButton
+                label="Submit"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.handleClose}
+            />,
+        ];
+        return (
             <div className="flex-container">
-                <AddPost/>
-                <PostList/>
+                <div>
+                    <RaisedButton label="Scrollable Dialog" onClick={this.handleOpen} />
+                    <Dialog
+                        title="Scrollable Dialog"
+                        actions={actions}
+                        modal={false}
+                        open={this.state.open}
+                        onRequestClose={this.handleClose}
+                        autoScrollBodyContent={true}
+                    >
+                        <AddPost />
+                    </Dialog>
+                </div>
+
+                <PostList />
             </div>
         );
     }
