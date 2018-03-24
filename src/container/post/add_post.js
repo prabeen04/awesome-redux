517729f88d4787f725385bfaa21c48b56400848c
Prabeen04 <prabeen.strange@gmail.com>
2018-03-24 20:12:33 +0530

    addpost component created

---
+++ src/container/post/add_post.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddPost extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+      </div>
+    )
+  }
+}
+
+export default AddPost;
--- src/container/post/post_list.js
+++ src/container/post/post_list.js
@@ -27,7 +27,6 @@ class PostList extends React.Component {
             return(<div>Loading...</div>)
         }
         return (<div className="">
-            <h1>post list</h1>
             {renderPost}
         </div>
 
